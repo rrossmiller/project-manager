d29038653f32e89923662f531c1cc37416a0cf0e
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-11-30 20:54:34 -0500

    add proj with default path ✅

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 clear
 cd src
-go run main.go
+go run main.go $@ #testProject
--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -9,67 +10,119 @@ import (
 	// "github.com/fatih/color"
 )
 
-const defaultPath = "~/Documents"
+const defaultPath = "~/Documents/Projects"
 
 // var red = color.New(color.FgRed).SprintFunc()
 // var blue = color.New(color.FgBlue).SprintFunc()
 
 type Project struct {
-	Name string
-	Path string
+	Name        string
+	Path        string
+	IsCommented bool
 }
 
 func main() {
-	// customPath := flag.Bool("c", false, "Custom path to add project")
-	// remove := flag.Bool("r", false, "remove project")
-	// regex := flag.Bool("r", false, "regex")
+	customPath := flag.Bool("c", false, "Custom path to add project")
+	remove := flag.Bool("r", false, "Remove project")
 	flag.Parse()
-	// args := flag.Args()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.CommandLine.Usage()
+		Err("", 0)
+	}
 
 	// read current zshalias file
 	b, err := os.ReadFile("../.zshalias")
-	check(err)
-	lines := string(b)
+	Check(err)
+	lines := strings.Split(string(b), "\n")[1:]
 	projects := make([]Project, 0)
 
-	for i, line := range strings.Split(lines, "\n") {
+	for i, line := range lines {
 		if utf8.RuneCountInString(line) > 0 {
-			line = strings.TrimSpace(line)
-			if line[0] != '#' {
-				proj := getProjectFromLine(i, line)
-				projects = append(projects, proj)
+			isCommented := false
+			if line[0] == '#' {
+				isCommented = true
+				line = line[1:]
 			}
+
+			line = strings.TrimSpace(line)
+
+			proj := GetProjectFromLine(i, line)
+			proj.IsCommented = isCommented
+			projects = append(projects, proj)
 		}
 	}
 
 	fmt.Println(projects)
 
+	// add project from user input
+	if *remove {
+		fmt.Println("remove")
+	} else {
+		var path string
+		if *customPath {
+			path = args[1]
+		} else {
+			path = defaultPath
+		}
+		projects = AddProject(args[0], path, projects)
+	}
 	// write updated file
+	WriteAliasProjectFile(projects)
 }
 
-func getProjectFromLine(i int, line string) Project {
+func GetProjectFromLine(i int, line string) Project {
 	splits := strings.Split(line, "=")
 	if len(splits) < 2 {
-		fmt.Printf("Line %d is malformed. Should contain '='\n", i+1)
-		os.Exit(0)
+		Err(fmt.Sprintf("Line %d is malformed. Should contain '='\n", i+1), 0)
 	}
 	// get name
 	alias := strings.Split(splits[0], " ")
 	if alias[0] != "alias" {
-		fmt.Printf("Line %d is malformed. Should start with alias\n", i+1)
-		os.Exit(0)
+		Err(fmt.Sprintf("Line %d is malformed. Should start with alias\n", i+1), 0)
 	}
 	name := alias[1][2:]
 
 	// get path
 	pSplit := strings.Split(splits[1], "&&")[0]
 	pSplit = strings.Split(pSplit, " ")[1]
-	return Project{name, pSplit}
+	return Project{name, pSplit, false}
 }
 
-func check(e error) {
+func AddProject(name, path string, projects []Project) []Project {
+	if name[0] == '#' {
+		Err("Project name must not start with '#'\n", 0)
+	}
+	path = path + "/" + name
+	return append(projects, Project{name, path, false})
+}
+
+func WriteAliasProjectFile(projects []Project) {
+	f, err := os.Create("../.zshalias")
+	Check(err)
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	_, err = w.WriteString("# Project paths\n\n")
+	Check(err)
+	for _, p := range projects {
+		line := fmt.Sprintf("alias pp%v='cd %v && clear && work'\n", p.Name, p.Path)
+
+		if p.IsCommented {
+			line = "# " + line
+		}
+		w.WriteString(line)
+	}
+	w.Flush()
+}
+
+func Check(e error) {
 	if e != nil {
 		panic(e)
-		// log.Fatal("something went wrong reading the fileNames")
 	}
+}
+
+func Err(message string, code int) {
+	fmt.Print(message)
+	os.Exit(code)
 }
