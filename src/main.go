838c9bba7a3361cb4bd4b3ec805b59fff6d852f7
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-12-06 13:36:53 -0500

    add update functionality

--- src/main.go
+++ src/main.go
@@ -21,21 +21,32 @@ type Project struct {
 	Path        string
 	IsCommented bool
 }
+type Options struct {
+	CustomPath bool
+	Clean      bool
+	List       bool
+	Remove     bool
+	RemoveF    bool
+	Update     bool
+}
 
 func main() {
 	customPath := flag.Bool("c", false, "Custom path to add project")
 	clean := flag.Bool("clean", false, "Clear commented projects from the file")
-	list := flag.Bool("l", false, "List current projects and paths")
+	list := flag.Bool("l", false, "ListProjects current projects and paths")
 	remove := flag.Bool("r", false, "Remove project")
 	removeF := flag.Bool("rf", false, "Force remove project")
+	update := flag.Bool("U", false, "Update project list in ~/Documents/Projects")
 	flag.Parse()
 
 	args := flag.Args()
-	if (len(args) == 0 && !*clean) && !*list {
+	if (len(args) == 0 && !*clean) && !*list && !*update {
 		flag.CommandLine.Usage()
 		Err("", 0)
 	}
 
+	// check viable args
+	CheckArgs(Options{*customPath, *clean, *list, *remove, *removeF, *update})
 	// read current zshalias file
 	homeDir, err := os.UserHomeDir()
 	Check(err)
@@ -43,7 +54,7 @@ func main() {
 	b, err := os.ReadFile(path)
 	Check(err)
 	lines := strings.Split(string(b), "\n")[1:]
-	projects := make([]Project, 0)
+	projects := make([]*Project, 0)
 
 	for i, line := range lines {
 		if utf8.RuneCountInString(line) > 0 {
@@ -57,27 +68,55 @@ func main() {
 
 			proj := GetProjectFromLine(i, line)
 			proj.IsCommented = isCommented
-			projects = append(projects, proj)
+			projects = append(projects, &proj)
 		}
 	}
 
 	if *list {
-		List(projects)
+		ListProjects(projects)
 		os.Exit(0)
 	}
-	// add project from user input
-	if *remove || *removeF {
-		projects = RemoveProject(args[0], projects, *removeF)
-	} else if *clean {
-		projects = Clean(projects)
-	} else {
-		var path string
-		if *customPath {
-			path = args[1]
-		} else {
-			path = defaultPath
+
+	if *update {
+		// get all paths in ~/Documents/Projects
+		projsDir, err := os.ReadDir(homeDir + "/Documents/Projects")
+		Check(err)
+		for _, v := range projsDir {
+			found := false
+			projPath := "~/Documents/Projects/" + v.Name()
+			for _, p := range projects {
+
+				p.Path = strings.ReplaceAll(p.Path, "/Users/rrossmil", "~")
+				if p.Path[utf8.RuneCountInString(p.Path)-1] == '/' {
+					p.Path = p.Path[:utf8.RuneCountInString(p.Path)-1]
+				}
+				// if the paths match, use the name from the zshalias file
+				if projPath == p.Path {
+					found = true
+				}
+			}
+			// if the project wasn't already found, add it to the list of projects
+			if !found {
+				projects = append(projects, &Project{v.Name(), projPath, false})
+			}
 		}
-		projects = AddProject(args[0], path, projects)
+	}
+
+	// add project from user input
+	if !*update {
+		if *remove || *removeF {
+			projects = RemoveProject(args[0], projects, *removeF)
+		} else if *clean {
+			projects = Clean(projects)
+		} else {
+			var path string
+			if *customPath {
+				path = args[1]
+			} else {
+				path = defaultPath
+			}
+			projects = AddProject(args[0], path, projects)
+		}
 	}
 	// write updated file
 	WriteAliasProjectFile(path, projects)
@@ -87,12 +126,12 @@ func main() {
 func GetProjectFromLine(i int, line string) Project {
 	splits := strings.Split(line, "=")
 	if len(splits) < 2 {
-		Err(fmt.Sprintf("Line %d is malformed. Should contain '='\n", i+1), 0)
+		Err(fmt.Sprintf("Line %d is malformed. Should contain '='", i+1), 0)
 	}
 	// get name
 	alias := strings.Split(splits[0], " ")
 	if alias[0] != "alias" {
-		Err(fmt.Sprintf("Line %d is malformed. Should start with alias\n", i+1), 0)
+		Err(fmt.Sprintf("Line %d is malformed. Should start with alias", i+1), 0)
 	}
 	name := alias[1][2:]
 
@@ -102,10 +141,10 @@ func GetProjectFromLine(i int, line string) Project {
 	return Project{name, pSplit, false}
 }
 
-func RemoveProject(name string, projects []Project, force bool) []Project {
-	rtn := make([]Project, 0)
+func RemoveProject(name string, projects []*Project, force bool) []*Project {
+	rtn := make([]*Project, 0)
 	if name[0] == '#' {
-		Err("Project name must not start with '#'\n", 0)
+		Err("Project name must not start with '#'", 0)
 	}
 
 	foundName := false
@@ -121,13 +160,13 @@ func RemoveProject(name string, projects []Project, force bool) []Project {
 		}
 	}
 	if !foundName {
-		Err(fmt.Sprintf("Did not find project '%v'\n", name), 0)
+		Err(fmt.Sprintf("Did not find project '%v'", name), 0)
 	}
 	return rtn
 }
 
-func Clean(projects []Project) []Project {
-	rtn := make([]Project, 0)
+func Clean(projects []*Project) []*Project {
+	rtn := make([]*Project, 0)
 	for _, p := range projects {
 		if !p.IsCommented {
 			rtn = append(rtn, p)
@@ -136,9 +175,9 @@ func Clean(projects []Project) []Project {
 	return rtn
 }
 
-func AddProject(name, path string, projects []Project) []Project {
+func AddProject(name, path string, projects []*Project) []*Project {
 	if name[0] == '#' {
-		Err("Project name must not start with '#'\n", 0)
+		Err("Project name must not start with '#'", 0)
 	}
 	// if name is in projects, err
 	for i, p := range projects {
@@ -153,10 +192,10 @@ func AddProject(name, path string, projects []Project) []Project {
 	}
 
 	path = path + "/" + name
-	return append(projects, Project{name, path, false})
+	return append(projects, &Project{name, path, false})
 }
 
-func WriteAliasProjectFile(path string, projects []Project) {
+func WriteAliasProjectFile(path string, projects []*Project) {
 	f, err := os.Create(path)
 	Check(err)
 	defer f.Close()
@@ -174,7 +213,7 @@ func WriteAliasProjectFile(path string, projects []Project) {
 	w.Flush()
 }
 
-func List(projects []Project) {
+func ListProjects(projects []*Project) {
 	for _, p := range projects {
 		s := fmt.Sprintf("%v - %v\n", p.Name, p.Path)
 		if p.IsCommented {
@@ -185,6 +224,30 @@ func List(projects []Project) {
 	}
 }
 
+// type Options struct {
+// 	CustomPath bool
+// 	Clean      bool
+// 	List       bool
+// 	Remove     bool
+// 	RemoveF    bool
+// 	Update     bool
+
+// customPath := flag.Bool("c", false, "Custom path to add project")
+// clean := flag.Bool("clean", false, "Clear commented projects from the file")
+// list := flag.Bool("l", false, "ListProjects current projects and paths")
+// remove := flag.Bool("r", false, "Remove project")
+// removeF := flag.Bool("rf", false, "Force remove project")
+// update := flag.Bool("U", false, "Update project list in ~/Documents/Projects")
+// }
+
+func CheckArgs(opts Options) {
+	switch {
+	case opts.List && (opts.CustomPath || opts.Clean || opts.Remove || opts.RemoveF || opts.Update):
+		Err("Cannot list projects and do any other action", 0)
+		// case TODO others
+	}
+}
+
 func Check(e error) {
 	if e != nil {
 		panic(e)
@@ -192,23 +255,10 @@ func Check(e error) {
 }
 
 func Err(message string, code int) {
-	fmt.Print(message)
+	fmt.Println(message)
 	os.Exit(code)
 }
 
 func Finish() {
-
-	// binary, lookErr := exec.LookPath("source")
-	// if lookErr != nil {
-	// 	panic(lookErr)
-	// }
-	// args := []string{"rzshrc"}
-	// env := os.Environ()
-
-	// execErr := syscall.Exec(binary, args, env)
-	// if execErr != nil {
-	// 	panic(execErr)
-	// }
-
 	exec.Command("rzshrc").Output()
 }
